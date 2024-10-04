71fb94f4db3893be6fded369f9e02a1fe35c55fa
hasan814 <h.mousavi910@gmail.com>
2024-10-04 23:14:18 +0330

    Added Dependencies

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import express from "express";
+import cookieParse from "cookie-parser";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
