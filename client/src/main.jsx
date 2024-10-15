4ab897173d30e1a80932c9ca7d81746df0b39139
hasan814 <h.mousavi910@gmail.com>
2024-10-15 10:43:49 +0330

    modify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store/store.jsx";
 
 import "./index.css";
-import router from "./routes/Index.js";
+import router from "./routes/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
